e53c99e14a888051ac125c21f4262a78877b3002
Angythub <reyno.willy@gmail.com>
2015-04-21 23:54:24 +0200

    Add new API file

--- app/js/factory/questions.js
+++ app/js/factory/questions.js
@@ -1,7 +1,7 @@
 angular.module('dwQuestionsService', ['ngResource'])
-    .factory('QuestionFactory', ['$resource',
-        function($resource){
-            return $resource('http://localhost/dual-wine/public/api/question', {}, {
+    .factory('QuestionFactory', ['$resource', 'apiQuestions',
+        function($resource, apiQuestions){
+            return $resource(apiQuestions, {}, {
                 query: {method:'GET', params:{limit: 4}, isArray:true}
                 // Todo add a random parameter ?
             });
--- app/js/values/values.js
+++ app/js/values/values.js
@@ -1,7 +1,8 @@
 angular.module('dwValues', [])
-    .value('apiLogin', 'http://localhost/dual-wine/public/api/auth/login') //post
-    .value('apiRegister', 'http://localhost/dual-wine/public/api/user') //post
-    .value('apiUserUpdate', 'http://localhost/dual-wine/public/api/user') //put
+    .value('apiLogin', 'http://localhost/dual-wine/public/api/auth/login') // post
+    .value('apiRegister', 'http://localhost/dual-wine/public/api/user') // post
+    .value('apiUserUpdate', 'http://localhost/dual-wine/public/api/user') // put
+    .value('apiQuestions', 'http://localhost/dual-wine/public/api/question/random') // get
     .constant('AUTH_EVENTS', {
         loginSuccess: 'auth-login-success',
         loginFailed: 'auth-login-failed',
